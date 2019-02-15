d8af52de14045cf74fb5c4e1e41758876063a4a3
Julian WEBER <julian.weber@hotmail.fr>
2019-02-15 13:06:45 +0100

    Get stars

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
+const fetch = require("node-fetch");
 var hostelFectching = require("./getHostelList")
-var { loadHostels, updateHostels, onlyMichelin } = hostelFectching
+var { loadHostels, updateHostels } = hostelFectching
 
 async function startup(){
     hostels = await updateHostels()
-    //onlyMichelin(hostels)
-    //onlyMichelin([{name: "hello world"}, {name: "le relais"}])
+    //hostels = await loadHostels()
+    //hostels = await michelin(hostels)
+    console.log(hostels)
 }
 
-async function test(){
+async function testRelais(){
     var uri = "https://www.relaischateaux.com/us/france/albert-haute-savoie-chamonix-mont-blanc"
     const options = {
         uri: uri,
@@ -18,8 +20,17 @@ async function test(){
         }
     }
     var $ = await rp(options)
-    console.log($(".hotelHeader-img").attr('data-src'))
+    console.log($(".richTextMargin").text())
+}
+
+async function testMichelin(){
+    var response = await fetch('https://restaurant.michelin.fr/2clh5yg/le-parc-les-crayeres-reims')
+    response = await response.text()
+    if(response.includes("cotation3etoiles")){
+        console.log("yes")
+    }
+    console.log(response.includes("icon-cotation2etoiles"))
 }
 
 startup()
-//test()
+//testMichelin()
--- server/getHostelList.js
+++ server/getHostelList.js
@@ -35,46 +35,17 @@ module.exports = {
             console.log("Fetching each hostel details : " + Math.floor(i * 100 * 10 / links.length) / 10 + "%")
             temp = links.slice(i, i + chunk);
             hotels = hotels.concat(await batchRequestHostels(temp))
-            sleep(150)
+            //sleep(150)
         }
         fs.writeFileSync('hostels.json', JSON.stringify(hotels, null, 2));
         return hotels
-    },
-    onlyMichelin: async function(hostels){
-        batchRequestAutocompletion(hostels)
     }
 }
 
-async function batchRequestAutocompletion(hotels) {
-    let res = []
-    var counter = 0
-    for (let i = 0; i < hotels.length; i++) {
-        res.push(fetch("https://restaurant.michelin.fr/index.php?q=search/autocomplete/" + encodeURI(hotels[i].name)))
-    }
-    for (let i = 0; i < hotels.length; i++) {
-        var response = await res[i]
-        response = (await response.json())
-        if (response.toString().includes("Aucun résultat.")){
-            console.log("pas michelin")
-        }
-        else{
-            response = await JSON.stringify(response)
-            if(response.includes("poi")){
-                console.log("michelin")
-                counter += 1
-            }
-            else{
-                console.log("BUG")
-            }
-        }
-    }
-    console.log(counter)
-    return hotels
-}
 
 async function batchRequestHostels(uris) {
     let res = []
-    let hotels = []
+    let hostels = []
     for (let i = 0; i < uris.length; i++) {
         const options = {
             uri: uris[i],
@@ -88,6 +59,9 @@ async function batchRequestHostels(uris) {
     for (let i = 0; i < uris.length; i++) {
         $ = await res[i]
         let image = $(".hotelHeader-img").attr('data-src')
+        let citation = $(".citationMsg").text()
+        let location = $("span [itemprop='addressLocality']").text()
+        let description = deleteSpaces($(".richTextMargin").text().replace("\n", ""))
         var name = deleteSpaces($(".mainTitle2")["0"].children[0].data.split("\n")[1])
         name = he.decode(name)
         name = name.split(" – ")[0]
@@ -97,10 +71,19 @@ async function batchRequestHostels(uris) {
         sleep(20)
         let entityId = await getEntityId(url[url.length - 1])
         if (entityId != null) {
-            hotels.push({ name: name, uri: uris[i], entityId: entityId, image: "https:"+image})
+            hostels.push({
+                name: name,
+                uri: uris[i],
+                entityId: entityId,
+                image: "https:" + image,
+                citation: citation,
+                location: location,
+                description: description
+            })
         }
     }
-    return hotels
+    hostels = await michelin(hostels)
+    return hostels
 }
 
 async function getEntityId(hostel) {
@@ -125,6 +108,75 @@ async function getEntityId(hostel) {
     }
 }
 
+async function michelin(hostels) {
+    let res = []
+    for (let i = 0; i < hostels.length; i++) {
+        res.push(fetch("https://restaurant.michelin.fr/index.php?q=search/autocomplete/" + encodeURI(hostels[i].name.slice(0, -1))))
+    }
+    let trash = []
+    for (let i = 0; i < hostels.length; i++) {
+        var response = await res[i]
+        response = (await response.json())
+        //console.log(response)
+        if (response.toString().includes("Aucun résultat.")) {
+            trash.push(i)
+        }
+        else {
+            response = await JSON.stringify(response)
+            if (response.includes("poi")) {
+                response = await JSON.parse(response)
+                let keys = Object.keys(response)
+                let key = null
+                for (let j = keys.length - 1; j != -1; j--) {
+                    if (keys[j] != "poi-all" && keys[j].includes("poi")) {
+                        key = keys[j]
+                    }
+                }
+                if (key != null) {
+                    response = response[key].split('"')[1]
+                    hostels[i].michelinUri = "https://restaurant.michelin.fr" + response
+                }
+            }
+        }
+    }
+    hostels = removeNonMichelin(hostels)
+    hostels = await countMichelinStars(hostels)
+    return hostels
+}
+
+async function countMichelinStars(hostels) {
+    let res = []
+    for (let i = 0; i < hostels.length; i++) {
+        res.push(fetch(hostels[i].michelinUri))
+    }
+    let star = 0
+    for (let i = 0; i < hostels.length; i++) {
+        var response = await res[i]
+        response = await response.text()
+        star = 0
+        if (response.includes("icon-cotation1etoile")) {
+            star = 1
+        }
+        else if (response.includes("icon-cotation2etoiles")) {
+            star = 2
+        }
+        else if (response.includes("icon-cotation3etoiles")) {
+            star = 3
+        }
+        else if (response.includes("icon-cotation4etoiles")) {
+            star = 4
+        }
+        else if (response.includes("icon-cotation5etoiles")) {
+            star = 5
+        }
+        else if (response.includes("icon-cotation6etoiles")) {
+            star = 6
+        }
+        hostels[i].michelinStars = star
+    }
+    return hostels
+}
+
 
 function deleteSpaces(text) {
     while (text[0] == " ") {
@@ -137,4 +189,14 @@ function sleep(ms) {
     return new Promise(resolve => {
         setTimeout(resolve, ms)
     })
+}
+
+function removeNonMichelin(hostels) {
+    let result = []
+    for (let i = 0; i < hostels.length; i++) {
+        if (hostels[i].michelinUri != undefined) {
+            result.push(hostels[i])
+        }
+    }
+    return result
 }
